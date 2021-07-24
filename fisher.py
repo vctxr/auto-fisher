b482f5caf610b36f57b40b7486c7447fb8b212db
Victor Samuel Cuaca <victorsamuelcuaca131@gmail.com>
2021-07-24 16:24:01 +0700

    Update fisher.py

--- fisher.py
+++ fisher.py
@@ -3,12 +3,15 @@ import win32api, win32con
 import time
 import keyboard             # pip install keyboard
 
-# NOTE: ADJUST THESE 2 VALUES ACCORDING TO YOUR SETUP
+# NOTE: ADJUST THESE 4 VALUES ACCORDING TO YOUR SETUP
 # TO FIND OUT THIS VALUES UNCOMMENT THIS CODE TO CHECK THE BUTTON POSITION
 # AND TRIGGER COLOR
-button_pos = (784, 377) # XY position of the fishing button
-trigger_color = (156, 209, 100) # RGB
-color_range = 50
+# pyautogui.displayMousePosition()
+
+width, height = 50, 50          # scan area
+x_offset, y_offset = 760, 350   # offset from the top left corner
+trigger_color = (172, 231, 92)  # RGB color
+color_range = 15                # acceptable color range
 
 def click(x, y):
     win32api.SetCursorPos((x, y))
@@ -16,14 +19,44 @@ def click(x, y):
     time.sleep(.01) # delay the key up to ensure click is registered correctly
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+print('AUTO FISHER STARTED')
+print('>> PRESS Q TO STOP')
+
+def find_color(pixel_color, acceptable_range, picture, width, height):
+    for x in range(0, width):
+        for y in range(0, height):
+            r, g, b = picture.getpixel((x, y))
+
+            # RGB value checking in certain `acceptable` range
+            if (r in range(pixel_color[0] - acceptable_range, pixel_color[0] + acceptable_range) and 
+                g in range(pixel_color[1] - acceptable_range, pixel_color[1] + acceptable_range) and 
+                b in range(pixel_color[2] - acceptable_range, pixel_color[2] + acceptable_range)):
+                    return x, y
+
+    # iterated through all the pixels and no matching color found
+    return None, None
+
+iteration = 0
+last_x = 0
+last_y = 0
+
 while not keyboard.is_pressed('q'):
-    pixel = pyautogui.pixel(button_pos[0], button_pos[1])
+    iteration = (iteration + 1) % 1000
+    picture = pyautogui.screenshot(region=(x_offset, y_offset, width, height))
 
-    # RGB value checking in certain `acceptable` range
-    if (pixel[0] in range(trigger_color[0] - color_range, trigger_color[0] + color_range) and 
-        pixel[1] in range(trigger_color[1] - color_range, trigger_color[1] + color_range) and 
-        pixel[2] in range(trigger_color[2] - color_range, trigger_color[2] + color_range)):
+    # returns true if the given trigger color exists in the given picture
+    x, y = find_color(trigger_color, color_range, picture, width, height)
 
-        click(button_pos[0], button_pos[1])
-        time.sleep(4)   # delay to cast another fishing rod
-        click(button_pos[0], button_pos[1])
+    if x != None and y != None:
+        print(f'Button found at ({x}, {y}), iteration = {iteration}')
+        iteration = 0
+        last_x, last_y = x, y
+        click(x_offset + x, y_offset + y)
+        time.sleep(3.5)   # delay to cast another fishing rod
+        click(x_offset + x, y_offset + y)
+
+    # fallback if target color not found
+    if iteration > 200 and last_x != 0 and last_y != 0:
+        iteration = 0
+        print("Fallback triggered!")
+        click(x_offset + last_x, y_offset + last_y)
